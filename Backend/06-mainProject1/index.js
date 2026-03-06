7ba53f91a78aeaeea32b8158d3eb28fa22c5a2fe
gaurav <gouravkumar42242@gmail.com>
2026-03-06 19:18:14 +0530

    project1

--- Backend/06-mainProject1/index.js
+++ Backend/06-mainProject1/index.js
@@ -1,6 +1,7 @@
 import express from 'express'; 
 import Path  from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const app = express(); 
 const fileName = fileURLToPath(import.meta.url); 
@@ -18,7 +19,34 @@ app.set('view engine' , 'ejs');
 
 
 app.get('/' , ((req , res , next) => {
+    fs.readdir('./files' , ((err , files) => {
 
+        const allFiles = files.map((file) => {
+            const content = fs.readFileSync(`./files/${file}`, 'utf-8');
+
+            return {
+                title: file.replace(".txt", ""),
+                note: content
+            };
+        });
+
+        res.render('index' , {files: allFiles}); 
+    }))
+}))
+
+
+app.get('/file/:filename' , ((req , res , next) => {
+    fs.readFile(`./files/${req.params.filename}.txt` , "utf-8" , ((err , filedata) => {
+        const formattedData = filedata.replace(/\n/g, '<br>');
+        res.render('show' , {title: req.params.filename , note: formattedData})
+    }))
+}))
+
+
+app.post('/create' , ((req , res , next) => {
+    fs.writeFile(`files/${req.body.title.split(" ").join("") || "untitled" }.txt` , req.body.note , ((err) =>{
+        res.redirect('/')
+    }))
 }))
 
 app.listen(3000 , () => {
